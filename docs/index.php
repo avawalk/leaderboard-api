11176ba43c1c8ab47f592ef1896648f26d36bd8a
diewland <diewland@gmail.com>
2025-06-29 13:10:36 +0700

    support CORS

--- docs/index.php
+++ docs/index.php
@@ -1,4 +1,9 @@
 <?php
+    // Allow requests from any origin (not recommended for sensitive data)
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
     include './config.php';
     include './db.php';
 
